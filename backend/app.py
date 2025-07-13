cb5628edea77dee0aac585a1edfe6dadd749115d
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2025-07-13 08:26:10 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -335,4 +335,5 @@ def serve_gif(filename):
     return send_from_directory(app.config['GIF_OUTPUT_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
